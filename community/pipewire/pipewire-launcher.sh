0d350ec3bb26fc04d201f301c0da3948dac6fa1e
Pranjal Kole <pranjal.kole7@gmail.com>
2022-02-25 17:12:29 +0530

    community/pipewire: kill previous instance before launch
    /usr/bin/pipewire was not being killed previously.
    
    Additionally, the order in which the processes are killed has been
    changed to avoid this error message from pipewire-media-session:
    
    [W][77952.698669] ms.core      | [ media-session.c: 2211 core_error()] error id:0 seq:799 res:-32 (Broken pipe): connection error

--- community/pipewire/pipewire-launcher.sh
+++ community/pipewire/pipewire-launcher.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # We need to kill any existing pipewire instance to restore sound
-pkill -u "${USER}" -x pipewire 1>/dev/null 2>&1
 pkill -u "${USER}" -fx "pipewire -c pipewire-pulse.conf" 1>/dev/null 2>&1
 pkill -u "${USER}" -fx /usr/bin/pipewire-media-session 1>/dev/null 2>&1
 pkill -u "${USER}" -fx /usr/bin/wireplumber 1>/dev/null 2>&1
+pkill -u "${USER}" -fx /usr/bin/pipewire 1>/dev/null 2>&1
 
 exec /usr/bin/pipewire &
 
